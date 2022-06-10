738d495dd95ad46993dc35871d9be70b90b40da8
Berat Yuseinov <Berat@students.softuni.bg>
2022-06-10 15:35:22 +0300

    Made new custom Resource CustomRegistrationFormResource.bg.resx

---
+++ DummySite/Global.asax.cs
@@ -0,0 +1,56 @@
+﻿using DummySite.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Localization;
+
+namespace DummySite
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Bootstrapper.Bootstrapped += Bootstrapper_Bootstrapped;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+        private void Bootstrapper_Bootstrapped(object sender, EventArgs e)
+        {
+            // Register any Resource classes
+            Res.RegisterResource<CustomRegistrationFormResource>();
+        }
+    }
+}
---
+++ DummySite/Resources/CustomRegistrationFormResource.cs
@@ -0,0 +1,21 @@
+﻿using Telerik.Sitefinity.Localization;
+
+namespace DummySite.Resources
+{
+
+    [ObjectInfo("CustomRegistrationFormResource", ResourceClassId = "CustomRegistrationFormResource", Title = "CustomRegistrationFormResource", TitlePlural = "CustomRegistrationFormResourcePlural", Description = "Is used only in registration form widget")]
+    public class CustomRegistrationFormResource : Resource
+    {
+        [ResourceEntry("AlreadyRegistered",
+            Value = "Already registered?",
+            Description = "phrase: Already registered? in registration form",
+            LastModified = "2022/06/08")]
+        public string AlreadyRegistered
+        {
+            get
+            {
+                return this["AlreadyRegistered"];
+            }
+        }
+    }
+}
